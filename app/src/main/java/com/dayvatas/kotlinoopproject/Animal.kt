cade1a683eccd349fe9b64840987178cef5475f7
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-04-11 00:41:42 +0300

    static and dynamic polymorphism

---
+++ app/src/main/java/com/dayvatas/kotlinoopproject/Animal.kt
@@ -0,0 +1,7 @@
+package com.dayvatas.kotlinoopproject
+
+open class Animal {
+    open fun sing(){
+        println("animal class")
+    }
+}
--- app/src/main/java/com/dayvatas/kotlinoopproject/MainActivity.kt
+++ app/src/main/java/com/dayvatas/kotlinoopproject/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         println(myUser.age)
         println(myUser.name)
 
+        //encapsulation
         var james = Musician("James","Guitar",55)
         //james.age = 60
         println(james.age.toString())
@@ -22,9 +23,25 @@ class MainActivity : AppCompatActivity() {
         println(james.returnBandName("damla"))
         println(james.returnBandName("DDD"))
 
+        //inheritance
         var lars = SuperMusician("Lars", "Drums", 65)
         println(lars.name)
         println(lars.returnBandName("damla"))
         lars.sing()
+
+        //polymorphism
+        //static polymorphism
+        var mathematics = Mathematics()
+        println(mathematics.sum())
+        println(mathematics.sum(1,2))
+        println(mathematics.sum(1,2,3))
+
+        //dynamic polymorphism
+        val animal = Animal()
+        animal.sing()
+
+        val dog = Dog()
+        dog.test()
+        dog.sing()
     }
 }
